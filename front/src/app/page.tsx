3d9cbd89080bf896150e9acdae3e3980832830ea
Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2025-02-03 14:51:58 -0600

    otp

---
+++ front/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AuthPage from './pages/AuthPage'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>پروژه دیوار </h1>
+      <AuthPage />
+    </div>
+  )
+}
+
+export default Home
