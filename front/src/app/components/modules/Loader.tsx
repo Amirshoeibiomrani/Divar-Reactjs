673acf9baaca2cfd6c1adbb8551740e674878a55
Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2025-02-05 13:43:20 -0600

    episode 1 >>> end

---
+++ front/src/app/components/modules/Loader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='text-center mt-[150px]'><span className='w-[30px] h-[30px] m-auto border-4 border-solid border-[#ffc5c5] border-b-[#a62626] rounded-[50%] inline-block box-border animate-spin '></span></div>
+  )
+}
+
+export default Loader
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import AuthPage from './pages/AuthPage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const Home = () => {
+  const queryClient=new QueryClient({defaultOptions})
+
   return (
-    <div>
+    <QueryClientProvider client={queryClient} >
       <h1>پروژه دیوار </h1>
       <AuthPage />
-    </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+    
   )
 }
 
