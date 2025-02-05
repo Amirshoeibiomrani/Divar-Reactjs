1134d12fc7748f60d42737e1bce137e075cce2fc
Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2025-02-04 18:52:32 -0600

    config react query

--- front/src/app/components/templates/CheckOtpForm.tsx
+++ front/src/app/components/templates/CheckOtpForm.tsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import { checkOtp } from '@/app/services/auth'
+import { setCookie } from '@/app/utils/cookie'
+ 
 
 const CheckOtpForm = ({setCode, code, setStep,mobile}) => {
 
   const submitHandler=(event)=>{
     event.preventDefault()
     console.log({code,mobile})
+
+    if(code.length !==5) return
+    const {response,error}=await checkOtp(mobile, code)
+
+    if(response){
+      // console.log(response)
+      setCookie(response.data)
+      
+    }
+if(error)console.log(error.response.data.message)
   }
+
   return (
     
     <form onSubmit={submitHandler}>
+
       <p>تایید کد اس ام اس شده </p>
-      <span>کد پیامک شده به شماره <<{mobile}>> وارد کنید</span>
+      <span>کد پیامک شده به شماره  {mobile}  وارد کنید</span>
       <label htmlFor="input">کد تایید را وارد کنید</label>
       <input type="text" id='input' placeholder='کد تایید' value={code} onChange={(e)=>setCode(e.target.value)} />
       <button type='submit'>ورود</button>
       <button onClick={()=> setStep(1)}>تغییر شماره موبایل</button>
+
     </form>
   )
 }
